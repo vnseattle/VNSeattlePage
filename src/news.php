16146217f88d591152930cc9777b8990cddfde6f
Henry <vnseattleapp@gmail.com>
2021-01-06 16:03:51 -0700

    vns update

---
+++ src/news.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+
+<head>
+
+    <!-- Basic Page Needs
+================================================== -->
+    <title>Người Việt ở Seattle - Vietnamese in Seattle</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Người Việt ở Seattle - Vietnamese in Seattle">
+    <meta name="keywords"
+        content="Rao vặt, thuê nhà, người Việt Seattle, VNSeattle, Vietnamese in Seattle, Vietnamese in Washington">
+    <meta name="author" content="Henry Nguyen">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+================================================== -->
+    <link rel="icon" type="image/png" href="favicon.ico" />
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/main-color.css" id="colors">
+
+</head>
+
+<body class="transparent-header">
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Header Container
+================================================== -->
+        <header id="header-container">
+
+            <!-- Header -->
+            <div id="header">
+                <div class="container">
+
+                    <!-- Left Side Content -->
+                    <div class="left-side">
+
+                        <!-- Logo -->
+                        <div id="logo">
+                            <a href="index.html"><img src="images/logo.png" data-sticky-logo=" images/logo.png"
+                                    alt=""></a>
+                        </div>
+
+                        <!-- Mobile Navigation -->
+                        <div class="mmenu-trigger">
+                            <button class="hamburger hamburger--collapse" type="button">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </div>
+
+                        <!-- Main Navigation -->
+                        <nav id="navigation" class="style-1">
+                            <ul id="responsive">
+
+                                <li><a class="current" href="index.html">Home</a></li>
+
+                                <li><a href="page-list-classified-ads.html">Rao Vặt</a></li>
+
+                                <li><a href="/help/">Trợ Giúp</a></li>
+
+
+                                <li><a href="page-app.html">App</a></li>
+
+                            </ul>
+                        </nav>
+                        <div class="clearfix"></div>
+                        <!-- Main Navigation / End -->
+
+                    </div>
+                    <!-- Left Side Content / End -->
+
+
+                    <!-- Right Side Content / End -->
+                    <div class="right-side">
+                        <div class="header-widget">
+
+                        </div>
+                    </div>
+                    <!-- Right Side Content / End -->
+
+
+
+                </div>
+            </div>
+            <!-- Header / End -->
+
+        </header>
+        <div class="clearfix"></div>
+        <!-- Header Container / End -->
+
+
+        <!-- Banner
+================================================== -->
+
+
+        <!-- Content
+================================================== -->
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <h3 class="headline centered margin-top-75">
+                        <strong class="headline-with-separator">Doanh nghiệp, tổ chức, người Việt Seattle</strong>
+                    </h3>
+                </div>
+
+
+                <div class="col-md-12">
+
+                    <div class="grid">
+                        <div class="grid-sizer"></div>
+                        <div>
+                            <div class="grid-item">
+                                <img src="http://vnseattle.com/vnsapp/imgs/VISPosterFinalPhungLyBigger.png" />
+                            </div>
+                            <div class="grid-item">
+                                <img src="http://vnseattle.com/vnsapp/imgs/trieulaposter.jpg" />
+                            </div>
+                            <div class="grid-item">
+                                <img src="http://vnseattle.com/vnsapp/imgs/VinhPhonePoster1.jpg" />
+                            </div>
+                            <div class="grid-item">
+                                <img src="http://vnseattle.com/vnsapp/imgs/maytoyota.png" />
+                            </div>
+                            <div class="grid-item">
+                                <img src="http://vnseattle.com/vnsapp/imgs/Haanlogistic.jpg" />
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <!-- Info Section / End -->
+
+
+
+
+        <!-- Footer
+================================================== -->
+        <div id="footer" class="sticky-footer">
+            <!-- Main -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 col-sm-6">
+                        <img class="footer-logo" src="images/logo.png" alt="">
+                        <br><br>
+                        <p>
+                            Phát triển từ nhóm cộng đồng người Việt lớn nhất nước Mỹ. Chúng tôi không chỉ phục vụ Người
+                            Việt ở bang Washington, chúng tôi còn rất hoan nghênh sự gia nhập của các
+                            tiểu bang khác. Nhóm Facebook hiện nay đã có hơn 40,000
+                            thành viên tích cực.
+                        </p>
+                    </div>
+
+                    <div class="col-md-3  col-sm-12">
+                        <h4>Liên lạc</h4>
+                        <div class="text-widget">
+                            <span>Seattle, WA</span> <br>
+                            E-Mail:<span> <a href="#">vnseattleapp@gmail.com</a> </span><br>
+                        </div>
+
+                        <ul class="social-icons margin-top-20">
+                            <li><a class="facebook" href="http://fb.com/vnseattle"><i class="icon-facebook"></i></a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <!-- Copyright -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyrights">© 2020 VNSeattle LLC. All Rights Reserved.</div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- Footer / End -->
+
+
+        <!-- Back To Top Button -->
+        <div id="backtotop"><a href="#"></a></div>
+
+
+    </div>
+    <!-- Wrapper / End -->
+
+
+    <!-- Scripts
+================================================== -->
+    <script type="text/javascript" src="scripts/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery-migrate-3.1.0.min.js"></script>
+    <script type="text/javascript" src="scripts/mmenu.min.js"></script>
+    <script type="text/javascript" src="scripts/chosen.min.js"></script>
+    <script type="text/javascript" src="scripts/slick.min.js"></script>
+    <script type="text/javascript" src="scripts/rangeslider.min.js"></script>
+    <script type="text/javascript" src="scripts/magnific-popup.min.js"></script>
+    <script type="text/javascript" src="scripts/waypoints.min.js"></script>
+    <script type="text/javascript" src="scripts/counterup.min.js"></script>
+    <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="scripts/tooltips.min.js"></script>
+    <script type="text/javascript" src="scripts/custom.js"></script>
+    <script type="text/javascript" src="scripts/mansory.js"></script>
+
+
+    <!-- Leaflet // Docs: https://leafletjs.com/ -->
+    <script src="scripts/leaflet.min.js"></script>
+
+    <!-- Leaflet Maps Scripts -->
+    <script src="scripts/leaflet-markercluster.min.js"></script>
+    <script src="scripts/leaflet-gesture-handling.min.js"></script>
+
+    <!-- Leaflet Geocoder + Search Autocomplete // Docs: https://github.com/perliedman/leaflet-control-geocoder -->
+    <script src="scripts/leaflet-autocomplete.js"></script>
+    <script src="scripts/leaflet-control-geocoder.js"></script>
+
+
+    <!-- Typed Script -->
+    <script type="text/javascript" src="scripts/typed.js"></script>
+    <script>
+        var typed = new Typed('.typed-words', {
+            strings: ["Doanh nghiệp", " việc làm ", "nhà", " tổ chức cộng đồng", " địa điểm vui chơi", " sự kiện"],
+            typeSpeed: 50,
+            backSpeed: 80,
+            backDelay: 100,
+            startDelay: 500,
+            loop: true,
+            showCursor: true
+        });
+
+
+    </script>
+
+
+    <script>
+
+
+
+        $("#find").click(() => {
+
+            window.location.href = `page-list-search.html?q=${$("#key-word").val()}`;
+        });
+
+        $("#key-word").keypress(function () {
+            if (event.which == 13) {
+                window.location.href = `page-list-search.html?q=${$("#key-word").val()}`;
+
+            }
+
+        });
+
+        $('.grid').masonry({
+            // set itemSelector so .grid-sizer is not used in layout
+            itemSelector: '.grid-item',
+            // use element for option
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
+
+
+
+    </script>
+
+
+    <!-- Style Switcher
+================================================== -->
+    <script src="scripts/switcher.js"></script>
+
+    <div id="style-switcher">
+        <h2>Color Switcher <a href="#"><i class="sl sl-icon-settings"></i></a></h2>
+
+        <div>
+            <ul class="colors" id="color1">
+                <li><a href="#" class="main" title="Main"></a></li>
+                <li><a href="#" class="blue" title="Blue"></a></li>
+                <li><a href="#" class="green" title="Green"></a></li>
+                <li><a href="#" class="orange" title="Orange"></a></li>
+                <li><a href="#" class="navy" title="Navy"></a></li>
+                <li><a href="#" class="yellow" title="Yellow"></a></li>
+                <li><a href="#" class="peach" title="Peach"></a></li>
+                <li><a href="#" class="beige" title="Beige"></a></li>
+                <li><a href="#" class="purple" title="Purple"></a></li>
+                <li><a href="#" class="celadon" title="Celadon"></a></li>
+                <li><a href="#" class="red" title="Red"></a></li>
+                <li><a href="#" class="brown" title="Brown"></a></li>
+                <li><a href="#" class="cherry" title="Cherry"></a></li>
+                <li><a href="#" class="cyan" title="Cyan"></a></li>
+                <li><a href="#" class="gray" title="Gray"></a></li>
+                <li><a href="#" class="olive" title="Olive"></a></li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- Style Switcher / End -->
+
+
+</body>
+
+</html>
